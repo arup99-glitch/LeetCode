628d1ef5b1bbc8e0a4df4a12ff58ef1dca1fb88a
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2023-02-25 02:34:20 +0600

    Add files via upload

---
+++ maxOccurence.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char getMaxOccCharacter(string s){
+  int arr[26] = {0};
+
+  //create an array of count of characters
+  for(int i=0;i<s.length();i++)
+  {
+      char ch = s[i];
+      //lowercase
+      int number =0;
+      number = ch-'a';
+      arr[number]++;
+  }
+  //find maximum occur character
+  int maxi = -1, ans =0;
+  for(int i=0;i<26;i++){
+    if(maxi < arr[i]){
+        ans = i;
+        maxi = arr[i];
+    }
+  }
+  return 'a'+ans;
+
+}
+
+int main()
+{
+
+    string s;
+    cin>>s;
+    cout<<getMaxOccCharacter(s)<<endl;
+    return 0;
+}
